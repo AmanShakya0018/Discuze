dccf7e9a4a69f33113fec34cbbc9544ad7117e00
AmanShakya0018 <amanshakya1808@gmail.com>
2025-01-24 00:04:33 +0530

    added post create input on main page

--- app/(posts)/allposts/page.tsx
+++ app/(posts)/allposts/page.tsx
@@ -23,6 +23,8 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label"; import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { toast } from "@/hooks/use-toast";
 
 interface Post {
   id: string;
@@ -48,12 +50,16 @@ interface Comment {
 }
 
 const AllPosts = () => {
+  const { data: session } = useSession();
   const [posts, setPosts] = useState<Post[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string>("");
   const [openDialog, setOpenDialog] = useState<boolean>(false);
   const [selectedPostId, setSelectedPostId] = useState<string | null>(null);
   const [linkCopied, setLinkCopied] = useState<boolean>(false);
+  const [content, setContent] = useState("");
+
+  const [postloading, setPostLoading] = useState(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -70,6 +76,63 @@ const AllPosts = () => {
 
     fetchPosts();
   }, []);
+
+  const handlePostSubmit = async () => {
+
+    if (!session) {
+      toast({
+        description: "Login to create a post"
+      })
+      return;
+    }
+
+    if (!content) {
+      toast({
+        description: "Content is required"
+      })
+      return;
+    }
+
+    setPostLoading(true);
+
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/create`,
+        {
+          userId: session?.user.id,
+          content,
+        }
+      );
+
+      if (response.data.success) {
+        toast({
+          description: "Post added successfully"
+        })
+        setContent("");
+        const updatedPosts = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/allposts`
+        );
+        setPosts(updatedPosts.data);
+      } else {
+        toast({
+          description: response.data.message || "Something went wrong"
+        })
+      }
+    } catch (error) {
+      toast({
+        description: `failed to add post. Please try again ${error}`
+      })
+    } finally {
+      setPostLoading(false);
+    }
+  };
+
+  const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = e.target.value;
+    if (value.length <= 799) {
+      setContent(value);
+    }
+  };
 
 
   const handleShare = (postId: string) => {
@@ -92,11 +155,74 @@ const AllPosts = () => {
 
   return (
     <div className="min-h-fit text-neutral-800 dark:text-neutral-100">
-      <div className="max-w-2xl mx-auto">
+      <div className="max-w-2xl mx-auto mt-8">
+        {/*  */}
+        <div className="flex py-4 px-6 bg-zinc-50 dark:bg-zinc-900/40 border rounded-xl mb-6">
+          <div className="flex gap-3 flex-grow overflow-hidden">
+            <Image
+              width={48}
+              height={48}
+              src={session?.user.image || "/pfp.png"}
+              alt={session?.user.name || "pfp.png"}
+              className="w-10 h-10 rounded-full object-cover flex-shrink-0"
+            />
+            <div className="flex-1 min-w-0 overflow-hidden">
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col sm:flex-row sm:items-center gap-2 text-sm truncate">
+                  <span className="font-bold truncate">{session?.user.name}</span>
+                </div>
+              </div>
+              <div className="flex flex-col text-[0.85rem] text-neutral-800 dark:text-neutral-200 whitespace-pre-wrap break-words overflow-hidden">
+                <div>
+                  <textarea
+                    placeholder="What is happening?!"
+                    value={content}
+                    onChange={handleContentChange}
+                    className="border bg-neutral-100 dark:bg-neutral-950  border-neutral-200 dark:border-neutral-800 rounded-md p-2 mt-2 w-full max-w-full h-24"
+                  ></textarea>
+                </div>
+                <div className="flex items-center justify-between">
+                  <span className="text-xs sm:text-sm text-gray-500 mb-2">
+                    {content.length}/799 characters
+                  </span>
+                  <button className="text-sm text-neutral-700 dark:text-neutral-300 hover:text-neutral-500 border px-3 py-1 rounded-lg"
+                    onClick={handlePostSubmit}
+                    disabled={loading}
+                  >
+                    {postloading ? (
+                      <div role="status" className="flex items-center h-5 w-6">
+                        <svg
+                          aria-hidden="true"
+                          className="w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-white ml-1"
+                          viewBox="0 0 100 101"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"
+                          />
+                          <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"
+                          />
+                        </svg>
+                        <span className="sr-only">Loading...</span>
+                      </div>
+                    ) : (
+                      "Post"
+                    )}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/*  */}
         {posts.length === 0 ? (
           <p className="text-center text-neutral-500">No posts available.</p>
         ) : (
-          <div className="flex flex-col gap-3 mt-8 py-2">
+          <div className="flex flex-col gap-3 py-2">
             {posts.map((post) => (
               <div key={post.id}>
                 <div className="flex py-4 px-6 bg-zinc-50 dark:bg-zinc-900/40 border rounded-xl">
