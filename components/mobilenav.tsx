8e187e94a4d95153263698dac7525e7f44b3cbae
AmanShakya0018 <amanshakya1808@gmail.com>
2025-02-27 15:40:37 +0530

    minor changes

--- components/mobilenav.tsx
+++ components/mobilenav.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-import { Home, User2 } from 'lucide-react'
+import { Home, Search, User2 } from 'lucide-react'
 
 const MobileNav = () => {
 
@@ -9,6 +9,9 @@ const MobileNav = () => {
       <div className="max-w-[80remrem] mx-auto text-sm text-gray-400 flex flex-row justify-between items-start ">
         <Link href="/" className="flex flex-col items-center justify-center border-r border-neutral-100 dark:border-white/[0.1] text-center text-neutral-500 w-1/2 h-12">
           <Home className='w-7 h-7' />
+        </Link>
+        <Link href="/search" className="flex flex-col items-center justify-center border-l border-neutral-100 dark:border-white/[0.1] text-center text-neutral-500 w-1/2 h-12">
+          <Search className='w-7 h-7' />
         </Link>
         <Link href="/profile" className="flex flex-col items-center justify-center border-l border-neutral-100 dark:border-white/[0.1] text-center text-neutral-500 w-1/2 h-12">
           <User2 className='w-7 h-7' />
